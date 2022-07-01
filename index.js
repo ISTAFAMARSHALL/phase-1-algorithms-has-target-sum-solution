d9cb32ec2ddf7ee7765ccdbcf720fabc4bde20a8
+ <+>
2022-07-01 04:32:44 -0400

    commit tree 905455f0476419d1e30d1cf21130ce86d63eba81

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  // for (let i = 0; i < array.length; i++) {
+
+  //   const complement = target - array[i] 
+
+  //   for (let j = i + 1; j < array.length; j++) {
+
+  //     if (array[j] === complement) return true
+  //   }
+  // }
+  // return false
+const seenNumbers = {}
+
+  for (let i = 0; i < array.length; i++ ) {
+
+    
+    const complement = target - array[i]
+
+    if (seenNumbers[complement]) return true
+
+    seenNumbers[array[i]] = true
+    console.log(seenNumbers[complement])
+
+  }
+
+  return false
+
 }
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
-*/
+    Runtime:  O(n^2)
+    Space:    O(n)
+
+
+  */  
 
 /* 
   Add your pseudocode here
